d35c3e46ec75971d28953fd7f5de2becc417547f
hajungin <hji000426@gmail.com>
2024-07-26 15:18:51 +0900

    # 충돌 해결 및 병합 완료

--- src/main/java/com/example/shoppingMall/ShoppingMallApplication.java
+++ src/main/java/com/example/shoppingMall/ShoppingMallApplication.java
@@ -2,11 +2,11 @@ package com.example.shoppingMall;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude={SecurityAutoConfiguration.class})
-//로그인페이지 비활성화
-//@SpringBootApplication
-public class ShoppingMallApplication {
+@SpringBootApplication
+public class  ShoppingMallApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppingMallApplication.class, args);
+    }
 
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,3 @@
-
 INSERT INTO category (category_code, category_name)
 VALUES
 ('1', '전자제품'),
@@ -22,43 +21,39 @@ VALUES
 ('3', '2', "/productImg/main044a3515-350a-42f5-b5c5-5ca15ec6c1ca.jpg"),
 ('4', '2', "/productImg/sub2ada9c40-c416-44e0-8223-0a0a134ecb45.jpg");
 
-INSERT INTO user (id, password,user_role)
-VALUES
-('1', '1111' ,'ADMIN'),
-('2', '1111', 'SELLER'),
-('3', '1111', 'USER');
-
-INSERT INTO user_info (user_info_code, user_name ,RRN, phone_number, created_date, email,birthdate, grade, id , is_active)
-VALUES
-('1', '가' ,'990708-0000000','010-1111-1111','2024-07-11', 'a@zzzz.com','1999-07-08','일반','1','Y'),
-('2', '나' ,'990718-1111111','010-1111-2222','2024-07-12', 'b@zzzz.com','1999-07-18','일반','1','Y'),
-('3', '다' ,'990728-2222222','010-1111-3333','2024-07-13', 'c@zzzz.com','1999-07-28','VIP','1','Y');
-
-INSERT INTO delivery (user_info_code, delivery_code ,postal_code, address, contact_number, contact_name)
-VALUES
-('1', '1' ,'000000','서울', '010-1111-1111','가'),
-('2', '2' ,'111111','인천', '010-1111-2222','나'),
-('3', '3' ,'222222','부산', '010-1111-3333','다');
-
-INSERT INTO orders (order_code, product_code, user_info_code, order_quantity, order_status, delivery_code)
-VALUES
-('1', '1' ,'1','1','배송중', '1'),
-('2', '2' ,'2','2','준비중', '2'),
-('3', '3' ,'3','3','배송완료', '3');
-
-INSERT INTO cart (cart_code, product_code, user_info_code, quantity)
-VALUES
-('1', '4' ,'1','1'),
-('2', '5' ,'2','2'),
-('3', '6' ,'3','3');
-
-
-
-
-
 -- user 암호 1111
 INSERT INTO users (id, password, user_role) VALUES
 ('admin', '$2a$10$gmwpP9rsFw25ilwcam.4s.eVil26pFMDZkbUlcAGcaxVW/CqNd2cS', 'ADMIN'),
-('user', '$2a$10$gmwpP9rsFw25ilwcam.4s.eVil26pFMDZkbUlcAGcaxVW/CqNd2cS', 'USER'),
+('user1', '$2a$10$gmwpP9rsFw25ilwcam.4s.eVil26pFMDZkbUlcAGcaxVW/CqNd2cS', 'USER'),
+('user2', '$2a$10$gmwpP9rsFw25ilwcam.4s.eVil26pFMDZkbUlcAGcaxVW/CqNd2cS', 'USER'),
+('user3', '$2a$10$gmwpP9rsFw25ilwcam.4s.eVil26pFMDZkbUlcAGcaxVW/CqNd2cS', 'USER'),
 ('seller', '$2a$10$gmwpP9rsFw25ilwcam.4s.eVil26pFMDZkbUlcAGcaxVW/CqNd2cS', 'SELLER');
+
+INSERT INTO user_info (user_info_code, user_name ,RRN, phone_number, created_date, email,birth_date, grade, id , is_active)
+VALUES
+('1', '가' ,'990708-0000000','010-1111-1111','2024-07-11', 'a@zzzz.com','1999-07-08','일반','admin','Y'),
+('2', '다' ,'990728-2222222','010-1111-3333','2024-07-13', 'c@zzzz.com','1999-07-28','일반','seller','Y'),
+('3', '나' ,'990718-1111111','010-1111-2222','2024-07-12', 'b@zzzz.com','1999-07-18','일반','user1','Y'),
+('4', '다' ,'990628-2222221','010-1111-3333','2024-07-13', 'c@zzzz.com','1999-06-28','VIP','user2','Y'),
+('5', '다' ,'990528-2222223','010-1111-3333','2024-07-13', 'c@zzzz.com','1999-05-28','VIP','user3','Y');
+
+INSERT INTO delivery (user_info_code, delivery_code ,postal_code, address, contact_number, contact_name)
+VALUES
+('3', '1' ,'000000','서울', '010-1111-1111','가'),
+('4', '2' ,'111111','인천', '010-1111-2222','나'),
+('5', '3' ,'222222','부산', '010-1111-3333','다');
+
+INSERT INTO orders (order_code, product_code, user_info_code, order_quantity, order_status, delivery_code)
+VALUES
+('1', '1' ,'3', '4','배송중', '1'),
+('2', '2' ,'4', '5','준비중', '2'),
+('3', '3' ,'5', '6','배송완료', '3');
+
+INSERT INTO cart (cart_code, product_code, user_info_code, quantity)
+VALUES
+('1', '1' ,'3','1'),
+('2', '2' ,'4','2'),
+('3', '3' ,'5','3');
+
+
 
