d412fa46a97a406246bb47fa51acd7617a50e400
2222nr <230320.nr.lee@gmail.com>
2024-08-10 01:08:13 +0900

    # 회원정보수정, 회원가입 수정

--- src/main/resources/static/js/user.js
+++ src/main/resources/static/js/user.js
@@ -1,21 +1,29 @@
 $(document).ready(function() {
 
+    $(".cancelBtn").click(function() {
+        window.history.back();
+    });
+
     $("input[name='role']").on("change", function() {
         if ($("#role_seller").is(":checked")) {
-            confirm("회원가입 후 관리자의 승인이 필요합니다.");
+            confirm("'SELLER'는 회원가입완료 후 관리자의 승인이 필요합니다.");
         }
     });
 
     var checkOk = false;
-    $('#password2').on('input', function() {
+
+    $('#password, #password2').on('input', function() {
         var password = $('#password').val();
         var password2 = $('#password2').val();
-        if (password !== password2) {
-            $('#passwordError').text('비밀번호가 일치하지 않습니다.');
+        if (password.length === 0 && password2.length === 0) {
+                $('#passwordError').text('');
+        } else if (password !== password2) {
+            $('#passwordError').text('비밀번호가 일치하지 않습니다.').css('color', 'red');
         } else {
-            $('#passwordError').text('');
+            $('#passwordError').text('비밀번호가 일치합니다.').css('color', 'blue');
         }
     });
+
     $('#id').on('input', function() {
         checkOk = false;
     });
@@ -27,7 +35,7 @@ $(document).ready(function() {
             alert("아이디는 4~20자로 입력해주세요");
         } else {
             $.ajax({
-                url: '/user/checkId',
+                url: '/checkId',
                 type: 'POST',
                 data: { id: userId },
                 success: function(response) {
@@ -51,23 +59,6 @@ $(document).ready(function() {
         var domain = $("#domain").val();
         var password = $('#password').val();
         var password2 = $('#password2').val();
-// 보류
-//        $.ajax({
-//            url: '/user/checkDuplicate',
-//            type: 'POST',
-//            data: { id: userId },
-//            success: function(response) {
-//                if(response.isDuplicate) {
-//                    alert("아이디가 중복되었습니다. 다른 아이디를 사용하세요.");
-//                } else {
-//                    alert("사용 가능한 아이디입니다.");
-//                    checkOk = true;
-//                }
-//            },
-//            error: function() {
-//                alert("아이디 중복 체크 중 오류가 발생했습니다. 다시 시도해 주세요.");
-//            }
-//        });
 
         if(checkOk === false) {
             alert("아이디 중복을 확인해주세요")
@@ -107,7 +98,7 @@ $(document).ready(function() {
         var phoneCheck = /^010-\d{4}-\d{4}$/;
         var phoneNumber = $("#phoneNumber").val();
         if(!phoneCheck.test(phoneNumber)) {
-            alert("전화번호를 확인해주세요")
+            alert("휴대폰번호를 확인해주세요")
             return false;
         }
         if($("#mailId").val().length === 0) {
@@ -134,8 +125,65 @@ $(document).ready(function() {
         console.log("Email: " + emailFull);
         $("#email").val(emailFull);
 
-        alert("입력이 완료되었습니다.")
-            $("form").submit()
+        alert("회원가입이 완료되었습니다.")
+            $("#signupForm").submit()
             return true;
+    });
+
+    $("#confirmBtn").click(function() {
+        var userId = $("#username").text();
+        var confirmPassword = $("#confirmPassword").val();
+        $.ajax({
+            url: '/user/checkPassword',
+            type: 'POST',
+            data: { userId: userId,
+                    password: confirmPassword },
+            success: function(response) {
+                if(response === "ok") {
+                    $(".confirmUser").hide();
+                    $(".modifyUserInfo").show();
+                } else {
+                    alert("비밀번호가 맞지 않습니다.");
+                }
+            },
+            error: function() {
+                alert("비밀번호 확인중 오류가 발생했습니다. 다시 시도해 주세요.");
+            }
+        });
+    });
+
+    $("#applySeller").on("change", function() {
+        if ($(this).prop("checked")) {
+            confirm("'SELLER'는 요청 후 관리자의 승인이 필요합니다.");
+        }
+    });
+
+    $("#applyCancel").on("change", function() {
+        if ($(this).prop("checked")) {
+            confirm("'SELLER' 승인 요청을 취소합니다.");
+        }
+    });
+
+    $("#save").click(function() {
+        var password = $("#password").val();
+        var password2 = $("#password2").val();
+        var phoneNumber = $("#phoneNumber").val();
+        var email = $("#email").val();
+        var phoneCheck = /^010-\d{4}-\d{4}$/;
+        var emailCheck = /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i;
+        if (password !== password2) {
+            alert("비밀번호를 확인해주세요")
+            $("#password2").focus();
+            return false;
+        } else if(!phoneCheck.test(phoneNumber)) {
+            alert("휴대폰번호를 확인해주세요");
+            return false;
+        } else if (!emailCheck.test(email)) {
+            alert("이메일을 확인해주세요");
+            return false;
+        }
+        alert("회원 정보 수정이 완료되었습니다.");
+        $("#userModifyForm").submit();
+        return true;
     });
 });
