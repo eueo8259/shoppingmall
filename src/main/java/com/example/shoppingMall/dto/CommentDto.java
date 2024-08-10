e4874ff9916b660366d3e49cc653a0673e5573aa
hajungin <hji000426@gmail.com>
2024-08-11 02:44:17 +0900

    문의 게시판 마무리

--- src/main/java/com/example/shoppingMall/dto/CommentDto.java
+++ src/main/java/com/example/shoppingMall/dto/CommentDto.java
@@ -1,5 +1,8 @@
 package com.example.shoppingMall.dto;
 
+import com.example.shoppingMall.entity.BulletinBoard;
+import com.example.shoppingMall.entity.Comment;
+import com.example.shoppingMall.entity.UserInfo;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,11 +11,21 @@ import java.time.LocalDate;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+
 public class CommentDto {
     private Long commentCode;
     private String commentText;
     private LocalDate creationDate;
-    private Long userInfoCode;
-    private Long boardCode;
+    private UserInfo userInfoCode;
+    private BulletinBoard boardCode;
 
+    public static CommentDto fromCommentEntity (Comment comment){
+        return new CommentDto(
+              comment.getCommentCode(),
+              comment.getCommentText(),
+              comment.getCreationDate(),
+              comment.getUserInfo(),
+              comment.getBoard()
+        );
+    }
 }
--- src/main/java/com/example/shoppingMall/entity/BulletinBoard.java
+++ src/main/java/com/example/shoppingMall/entity/BulletinBoard.java
@@ -1,13 +1,11 @@
 package com.example.shoppingMall.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Entity
-@Data
+@Setter
+@Getter
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
